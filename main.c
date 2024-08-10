48433997009076c242d1549cb8923913611f8f57
sasaplus1 <sasaplus1@gmail.com>
2024-08-10 23:18:04 +0900

    simplify

--- main.c
+++ main.c
@@ -1,28 +1,28 @@
 #include "pico/stdlib.h"
 
 const uint LED = 25;
-const uint PIN = 16;
+
+void
+setup_led() {
+  gpio_init(LED);
+  gpio_set_dir(LED, GPIO_OUT);
+  gpio_put(LED, 0);
+}
+
+void
+toggle_led() {
+  int led = gpio_get(LED);
+  gpio_put(LED, led > 0 ? 0 : 1);
+}
 
 int
 main() {
   stdio_init_all();
 
-  gpio_init(LED);
-  gpio_set_dir(LED, GPIO_OUT);
-  gpio_put(LED, 0);
-
-  gpio_init(PIN);
-  gpio_set_dir(PIN, GPIO_OUT);
-  gpio_pull_up(PIN);
-  gpio_put(PIN, 1);
+  setup_led();
 
   while (1) {
-    int led = gpio_get(LED);
-    int pin = gpio_get(PIN);
-
-    gpio_put(LED, led > 0 ? 0 : 1);
-    gpio_put(PIN, pin > 0 ? 0 : 1);
-
+    toggle_led();
     sleep_ms(500);
   }
 
