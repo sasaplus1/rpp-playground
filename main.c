c359fcc356edbbac90db2b6e298399dc78453a16
sasaplus1 <sasaplus1@gmail.com>
2024-08-11 22:39:18 +0900

    studying
    ref: https://blueeyes.sakura.ne.jp/category/%e9%9b%bb%e5%ad%90%e5%b7%a5%e4%bd%9c/%e3%83%9e%e3%82%a4%e3%82%b3%e3%83%b3/rp2040/

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "hardware/irq.h"
 #include "hardware/gpio.h"
 #include "tusb.h"
+#include "bsp/board.h"
 #include "ssd1306.h"
 
 // LED {{{
@@ -87,6 +88,7 @@ void loop_oled() {
 // }}}
 
 // HID keyboard {{{
+/*
 const uint KEY1 = 2;
 const uint KEY2 = 3;
 const uint KEY3 = 4;
@@ -175,19 +177,117 @@ void gpio_callback(uint gpio, uint32_t events) {
     tud_hid_keyboard_report(0, 0, keycode);
   }
 }
+*/
+
+const uint8_t device_desc[] = {
+    18, // bLength
+    1, // bDescriptorType
+    0x10,
+    0x01, // bcdUSB
+    0x00, // bDeviceClass
+    0x00, // bDeviceSubClass
+    0x00, // bDeviceProtocol
+    CFG_TUD_ENDPOINT0_SIZE, // bMaxPacketSize0
+    0x34,
+    0x12, // idVendor
+    0xcd,
+    0xab, // idProduct,
+    0x00,
+    0x00, // bcdDevice
+    0x00, // iManufacturer
+    0x01, // iProduct
+    0x00, // iSerialNumber
+    0x01, // bNumConfigurations
+};
+
+const uint8_t conf_desc[] = {
+    9, // bLength
+    2, // bDescriptorType
+    9 + 9,
+    0, // wTotalLength
+    1, // bNumInterface
+    1, // bConfigurationValue
+    0, // iConfiguration
+    0x20, // bmAttributes
+    0x0F, // bMaxPower
+
+    // --- Interface ---
+    9, // bLength
+    4, // bDescriptorType
+    1, // bInterfaceNumber
+    0, // bAlternateSetting
+    0, // bNumEndpoints
+    0xFF, // bInterfaceClass
+    0xFF, // bInterfaceSubClass
+    0xFF, // bInterfaceProtocol
+    0, // iInterface
+};
+
+const uint16_t string_desc_lang[] = { // Index: 0
+    4 | (3 << 8), // bLength & bDescriptorType
+    0x411 // てきとーにja-JP
+};
+const uint16_t string_desc_product[] = { // Index: 1
+    16 | (3 << 8),
+    'R', 'a', 's', 'p', 'i', 'c', 'o'
+};
+
+uint8_t const *tud_descriptor_device_cb(void) {
+  return device_desc;
+}
+
+uint8_t const *tud_descriptor_configuration_cb(uint8_t index) {
+  return conf_desc;
+}
+
+uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
+  uint16_t const *ret = NULL;
+  switch(index) {
+      case 0:
+          ret = string_desc_lang;
+          break;
+      case 1:
+          ret = string_desc_product;
+          break;
+      default:
+          break;
+  }
+  return ret;
+}
+
+const uint8_t hid_report_desc[] = {
+  TUD_HID_REPORT_DESC_GENERIC_INOUT(1)
+};
+
+const uint8_t conf_desc[] = {
+  TUD_CONFIG_DESCRIPTOR(1, 1, 0, TUD_CONFIG_DESC_LEN + TUD_HID_INOUT_DESC_LEN, 0, 0x0F),
+  TUD_HID_INOUT_DESCRIPTOR(1, 0, 0, sizeof(hid_report_desc), 0x01, 0x81, 64, 0x0F)
+};
+
+uint8_t const *tud_hid_descriptor_report_cb(void) {
+    return hid_report_desc;
+}
+uint16_t tud_hid_get_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen) {
+    return 0;
+}
+void tud_hid_set_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
+    return;
+}
+
 // }}}
 
 int main() {
   stdio_init_all();
+  board_init();
   tusb_init();
 
   setup_led();
   setup_oled();
-  setup_hid();
+  // setup_hid();
 
   while (1) {
     tud_task();
-    hid_task();
+    // hid_task();
     loop_oled();
   }
 
